f9f85ea408f1554a623d5e99c8b99edd5fdc741e
446f6e6e79 <davidedona03@gmail.com>
2024-04-30 12:33:26 +0200

    Added makeFile. Reorganized TrisServer

--- src/TrisServer.c
+++ src/TrisServer.c
@@ -3,13 +3,21 @@
 #include <sys/shm.h> 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#include "errExit.h"
 
+#define BOARD_SIZE 9
+
+//Definizione prototipi
+void printBoard(char *, int);
 void terminazioneSicura();
+
 
 void firstSigIntHandler(int sig){
     printf("È stato premuto CTRL-C.\nUna seconda pressione comporta la terminazione!");
-    //Cambio ora il comportamento al
+    //Cambio ora il comportamento al segnale sigInt
+    signal(SIGINT, secondSigIntHandler);
 }
 
 void secondSigIntHandler(int sig){
@@ -17,12 +25,12 @@ void secondSigIntHandler(int sig){
     printf("Il gioco è stato terminato.\n");
     exit(0);
 }
-#define BOARD_SIZE 9
+
 
 int main(int argC, char * argV[]){
     
     char player1, player2;
-    
+    int timeOut;
     char *board;
     
     //Controllo sui parametri passati
@@ -30,8 +38,24 @@ int main(int argC, char * argV[]){
         printf("Usage: %s <timeout> <SimboloPlayer1> <SimboloPlayer2>\n", argV[0]);
         return 1;
     }
-    //Generazione della memoria condivisa
+    //Converto il valore di timeout in un intero
+    timeOut = atoi(argV[1]);
+    if(timeOut < 0){
+        errExit("TimeOut > 0");
+    }
+    //Verifico la lunghezza dei simboli inseriti
+
+    if(strlen(argV[2]) != 1 || strlen(argV[3])!= 1){
+        errExit("Simboli devono essere caratteri");
+    }
+    player1 = argV[2];
+    player2 = argV[3];
+
+    //Setto il nuvo comportamento dei segnali
+    signal(SIGINT, firstSigIntHandler);
+
     
+    //Generazione della memoria condivisa
     int shmid = shmget(69, sizeof(char)*BOARD_SIZE, 0666|IPC_CREAT);
     if(shmid < 0){
         printf("Errore nella generazione della memoria condivisa\n");
@@ -50,14 +74,23 @@ int main(int argC, char * argV[]){
     for(int i = 0; i < BOARD_SIZE; i++){
         board[i] = ' ';
     }
-    for(int i = 0; i < BOARD_SIZE; i++){
-        if(i == 3){
-            printf("\n-------\n");
-        }
-        printf("%c|", board[i]);
-    }
+
+    printBoard(board, BOARD_SIZE);
+
     printf("\n");
     //Chiusura e pulizia della memoria condivisa con annessi attach
     shmdt((void *) board);
     shmctl(shmid, IPC_RMID, NULL);
+}
+
+void terminazioneSicura(){
+
+}
+void printBoard(char * board, int boardSize){
+    for(int i = 0; i < boardSize; i++){
+        if(i && i % 3 == 0){
+            printf("\n-------\n");
+        }
+        printf("%c|", board[i]);
+    }
 }
