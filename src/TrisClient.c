4475d54e5b73c3b1fcc91ab421123eee9e5843f8
Davide <davidedona03@gmail.com>
2024-05-12 14:09:23 +0200

    Added print for the input string

--- src/TrisClient.c
+++ src/TrisClient.c
@@ -316,7 +316,7 @@ int getPlayIndex(){
             }
         }
         else{
-            printf("ERRORE PARSING\n");
+            printf("ERRORE PARSING: string letta %s\n", input);
         }
         
 
